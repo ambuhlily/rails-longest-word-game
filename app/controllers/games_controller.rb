cc351ec812e11f9a32112ea1c587b5aac03a2c56
Amber Chapman <alichappy@gmail.com>
2022-06-04 10:36:01 +1000

    basics done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,17 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     alphabet = ('a'..'z').to_a
-    @@letters = alphabet.sample(10)
+    $letters = alphabet.sample(10)
   end
 
   def score
-    @letters = @@letters
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:new]}"
+
+    html_file = URI.open(url).read
+    html_doc = JSON.parse(html_file)
+    @result = html_doc['found']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: 'games#new'
+
   get '/new', to: 'games#new'
 
   post '/score', to: 'games#score'
+
+  # get '/checkword', to: 'games#score'
 end
